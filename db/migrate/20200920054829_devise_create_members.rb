96c3faf2d4b786c33141774930952e9a5a5fc2a0
rie <kou.kotetsu@gmail.com>
2020-09-21 00:51:24 +0900

    change-migration_files

--- db/migrate/20200920054829_devise_create_members.rb
+++ db/migrate/20200920054829_devise_create_members.rb
@@ -4,8 +4,12 @@ class DeviseCreateMembers < ActiveRecord::Migration[5.2]
   def change
     create_table :members do |t|
       ## Database authenticatable
+      t.string :family_name, null: false
+      t.string :first_name, null: false
+      t.string :nick_name, null: false, unique: true
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.boolean :is_withdeawal_status, null: false
 
       ## Recoverable
       t.string   :reset_password_token
@@ -31,7 +35,6 @@ class DeviseCreateMembers < ActiveRecord::Migration[5.2]
       # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
-
 
       t.timestamps null: false
     end
--- db/migrate/20200920144644_create_dogs.rb
+++ db/migrate/20200920144644_create_dogs.rb
@@ -1,12 +1,12 @@
 class CreateDogs < ActiveRecord::Migration[5.2]
   def change
     create_table :dogs do |t|
-      t.integer :member_id
+      t.integer :member_id, null: false
+      t.string :kind, null: false
+      t.string :age, null: false
       t.string :name
       t.string :image_id
-      t.string :kind
       t.string :sex
-      t.string :age
 
       t.timestamps
     end
