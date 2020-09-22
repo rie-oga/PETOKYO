be510ef43409f11dd9f45e1a52e482dd19d10c4d
rie <kou.kotetsu@gmail.com>
2020-09-23 02:38:34 +0900

    draft-mypage-show-edit

--- app/controllers/member/members_controller.rb
+++ app/controllers/member/members_controller.rb
@@ -1,10 +1,12 @@
 class Member::MembersController < ApplicationController
   def show
+  	@dogs = current_member.dogs
   end
 
   def unsubscribe
   end
 
   def edit
+  	@user = User.find(params[:id])
   end
 end
