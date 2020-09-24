5153672d779a1bd0e000964f9ca86ce3a8906e8c
rie <kou.kotetsu@gmail.com>
2020-09-24 22:00:52 +0900

    ajax

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,7 +1,30 @@
 class Admin::GenresController < ApplicationController
   def index
+  	@genres = Genre.all
+  	@genre = Genre.new
+  	respond_to do |format|#respond_toメソッドを使って結果をどのフォーマットで返すかを指定
+  		format.html
+  		format.js#ajaxで実行すると、このformat.jsに該当するgenre.js.erbを返す
+  	end
   end
 
   def edit
+  end
+
+  def create
+  	@genre = Genre.new(genre_params)
+  	respond_to do |format|
+  		if @genre.save
+  			@genres = Genre.all
+  		   format.js#ajax使用箇所でsaveに成功すればformat.jsに該当するcreate.js.erbが実行される
+  		else
+  		   format.js {render :index}
+  		end
+  	end
+  end
+
+  private
+  def genre_params
+  	params.permit(:name)
   end
 end
