2e8020c96f13635d3b29ffb97218d3e8611ba5a3
rie <kou.kotetsu@gmail.com>
2020-11-11 16:56:10 +0900

    jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -181,3 +181,26 @@ $(document).on('turbolinks:load', function() {
     });
   });
 });
+
+// footer最下部へ（jQuery）//
+$(function(){
+  // DOM取得後、変数へ
+    var $ftr = $('#footer');
+  //window.innerHeightで、画面全体の高さを取得
+  //$ftr.offset().topで、ドキュメントの上からfooterまでの高さを取得
+  //$ftr.outerHeight()で、footer自身の高さを取得
+    if( window.innerHeight > $ftr.offset().top + $ftr.outerHeight() ){
+  //$ftr.offset().topと$ftr.outerHeight()の合計よりも、画面全体の高さが大きくなったら、styleを付け足す
+  //付け足す高さは、画面全体の高さ-footer自身の高さ。これで最下部に固定される
+      $ftr.attr({'style': 'position:fixed; top:' + (window.innerHeight - $ftr.outerHeight()) +'px;' });
+    }
+});
+
+// left_side_bar footerと被らないように //
+jQuery( window ).on( 'scroll', function() {
+  if ( 1000 < jQuery( this ).scrollBottom() ) { // 1000px以上スクロールしたら
+    jQuery( '#fix1 #fix2' ).addClass( 'm_fixed' );
+  } else {
+    jQuery( '#fix1 #fix2' ).removeClass( 'm_fixed' );
+  }
+});
