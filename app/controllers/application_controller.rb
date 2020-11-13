77f676722a90f4ca60a66c7c8a4fb120ebceb187
rie <kou.kotetsu@gmail.com>
2020-11-14 02:22:46 +0900

    rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,25 +8,27 @@ class ApplicationController < ActionController::Base
 
 	protected
 
-	  def after_sign_in_path_for(resource)
-	    if resource == current_admin
-	       member_spots_path
-	    elsif
-	       resource == current_member
-	       member_spots_path
-	    end
-	  end
+	  	def after_sign_in_path_for(resource)
+		    if
+		       resource == current_admin
+		       member_spots_path
+		    elsif
+		       resource == current_member
+		       member_spots_path
+		    end
+	  	end
 
-	  def after_sign_out_path_for(resource)
-	    if resource == :admin
-	       admin_session_path
-	    elsif
-	    	resource == :member
-	        root_path
-	    end
-	  end
+	  	def after_sign_out_path_for(resource)
+		    if
+		       resource == :admin
+		       admin_session_path
+		    elsif
+		    	resource == :member
+		        root_path
+		    end
+	  	end
 
-	  def configure_permitted_parameters
-	    devise_parameter_sanitizer.permit(:sign_up, keys: [:nick_name])
-	  end
+		def configure_permitted_parameters
+		    devise_parameter_sanitizer.permit(:sign_up, keys: [:nick_name])
+		end
 end
--- app/controllers/member/comments_controller.rb
+++ app/controllers/member/comments_controller.rb
@@ -33,21 +33,21 @@ class Member::CommentsController < ApplicationController
 	end
 
 	def update
-    	 if @comment.update(comment_params)
-      		redirect_to member_member_path(@comment.member)
-    	 else
+    	if @comment.update(comment_params)
+      	   redirect_to member_member_path(@comment.member)
+        else
     	 	flash[:notice] = "☆評価は必須です。"
       		render :edit
-    	 end
+    	end
 	end
 
 	private
-  	def comment_params
-    	params.require(:comment).permit(:comment, :rate, comment_images_images: [])
-  	end
+	  	def comment_params
+	    	params.require(:comment).permit(:comment, :rate, comment_images_images: [])
+	  	end
 
-  	def ensure_correct_user
-  		@comment = Comment.find(params[:id])
-  	end
+	  	def ensure_correct_user
+	  		@comment = Comment.find(params[:id])
+	  	end
 
 end
